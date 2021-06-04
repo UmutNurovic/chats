78a015d81eaecba5c9ee419c82dec11b838b70af
umutcan <akturkumutcan58@gmail.com>
2021-06-04 16:24:32 +0300

    Chats

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessage = document.querySelector('.chat-messages');
 const socket = io();
+// get username and room from
+
+
+console.log(username);
 socket.on('message',(message)=>{
  console.log(message);
  outputMessage(message);
@@ -26,9 +30,9 @@ chatForm.addEventListener('submit',e=>{
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML =`<p class="meta">Brad <span>9:12pm</span></p>
+    div.innerHTML =`<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-    ${message}</p>
+    ${message.text }</p>
     `;
     document.querySelector('.chat-messages').appendChild(div);
 }
