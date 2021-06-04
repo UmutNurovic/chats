12e6f15ccedc111c6e1e924d4e9c5e1ab9532d97
umutcan <akturkumutcan58@gmail.com>
2021-06-04 23:53:38 +0300

    da

--- connect/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_CONNECT,  {
-    useNewUrlParser: true,useUnifiedTopology: true , useCreateIndex:true,useFindAndModify:false})
-.then(()=>{
-    console.log("Veri Tabanına bağlanıldı");
-})
-.catch(hata=>console.log(`veri tabanı bağlantı hatası ${hata}`));
